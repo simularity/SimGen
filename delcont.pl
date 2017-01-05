e6b907b8e9042596afdf61996dc0a13a1f7a3651
Anne Ogborn <annie@thelginworks.com>
2017-01-04 19:06:39 -0800

    add delcont.pl

---
+++ delcont.pl
@@ -0,0 +1,27 @@
+main :-
+    reset(p, foo(3), Cont),
+    writeln(main_c),
+    reset(Cont, bar(4), NewCont),
+    writeln(main_d),
+    call(NewCont).
+
+p :-
+	writeln(p_e),
+	q,
+	writeln(p_f).
+p :-
+	writeln(p_g),
+	q,
+	writeln(p_h).
+
+q :-
+	writeln(q_a),
+	shift(foo(X)),
+	writeln(X),
+	shift(bar(Y)), % wrong because were not in reset
+	writeln(Y).
+
+
+
+% hey annie, are you not calling a continuation you're given?
+%
